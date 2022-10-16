efb732a964299a8fd3b68819cd03aa2625c4274c
ryanbirbeck <114341023+ryanbirbeck@users.noreply.github.com>
2022-10-16 21:05:17 +1000

    Complete prac 5 state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -10,9 +10,19 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
+# while state_code != "":
+#     if state_code in CODE_TO_NAME:
+#         print(state_code, "is", CODE_TO_NAME[state_code])
+#     else:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
+    except KeyError:
+        print("invalid")
     state_code = input("Enter short state: ").upper()
+
+
+for state in CODE_TO_NAME:
+    print(f"{state:3} is {CODE_TO_NAME[state]}")
